6be8fb9f44627553ae023026e1b2726dcedf1964
camille4426 <camilleperrin26@gmail.com>
2022-01-05 08:43:18 +0100

    c

--- Code/interVersExec.ads
+++ Code/interVersExec.ads
@@ -14,7 +14,23 @@ package interVersExec is
    --           Fichier en entree vide
    procedure intermediaire2execution(code_intermediaire : in File_Type);
 
-   procedure avantDebut(code_intermediaire : in File_Type);
+   --procedure declarationVariables (NOM A CHANGER)
+   --semantique: A partir d'un fichier en entree ecrit en code source, traite le bloc de déclaration des variables.
+   --parametres: nom : code_intermediaire    type :  Fichier     flux : entree
+   --pre-condition: XXXXXXX
+   --post-condition: XXXXXX
+   --exception: Fichier incoherent / Ne correspond pas a un fichier de type code intermediaire
+   --           Fichier en entree vide
+   procedure declarationVariables(code_intermediaire : in File_Type);
+
+      --procedure blocPrincipal (NOM A CHANGER)
+   --semantique: A partir d'un fichier en entree ecrit en code source, traite le bloc principal du programme.
+   --parametres: nom : code_intermediaire    type :  Fichier     flux : entree
+   --pre-condition: XXXXXXX
+   --post-condition: Le programme est execute sans erreur
+   --exception: Fichier incoherent / Ne correspond pas a un fichier de type code intermediaire
+   --           Fichier en entree vide
+   procedure blocPrincipal(code_intermediaire : in File_Type);
 
    private
       Type T_Noeud; 
