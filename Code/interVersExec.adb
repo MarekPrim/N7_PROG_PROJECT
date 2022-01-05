998b269f10bf6bbcfac3ce9b0ddf8e1bd7a2550c
camille4426 <camilleperrin26@gmail.com>
2022-01-05 08:45:11 +0100

    c

--- Code/interVersExec.adb
+++ Code/interVersExec.adb
@@ -6,7 +6,8 @@ package body code is
 
    procedure intermediaire2execution(code_intermediaire : in File_Type) is
    begin
-
+        declarationVariables(code_intermediaire);
+        blocPrincipal(code_intermediaire);
    end intermediaire2execution;
 
 
