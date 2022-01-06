4975e833a96a424d2d7fd55892bf0013a0ad0f36
camille4426 <camilleperrin26@gmail.com>
2022-01-06 09:59:08 +0100

    c

--- Code/2.InterversExec/tests/test_affectation_01.adb
+++ Code/2.InterversExec/tests/test_affectation_01.adb
@@ -1,11 +1,11 @@
 -- XXXX
 with ada.Integer_Text_IO ; use ada.Integer_Text_IO;
 with ada.Text_IO ; use ada.Text_IO;
-with interVersExec ;
+with codage/interVersExec ;
 --with sourceVersInter; use sourceVersInter;
 -- R0
 
-procedure test_interversexec is
+procedure test_affectation_01 is
     -- Packages de généricité (pour tableaux)
     package entiers is new interversexec(Element=> Integer,image => Integer'Image);
     use entiers;
@@ -72,8 +72,4 @@ New_Line;
 
     --***** test intermediaire2execution *****
 
-
-
-
-
-end test_interversexec;
+end test_affectation_01;
