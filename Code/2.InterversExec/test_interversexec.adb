89dd5f74d798075f3a64ba56b179a53e50f1ecd3
camille4426 <camilleperrin26@gmail.com>
2022-01-06 09:21:28 +0100

    c

--- Code/2.InterversExec/test_interversexec.adb
+++ Code/2.InterversExec/test_interversexec.adb
@@ -17,10 +17,18 @@ procedure test_interversexec is
     
 
 begin
+Put_line("");
+New_Line;
+
 
     --***** test affectation ******
-    Put_Line ("Test d'affectation : ");
-    --affectation();
+    Put_Line ("Tests d'affectation : ");
+    New_Line;
+
+    -- ## ATTENTION, ce test doit prendre en compte qu'affectation vérifie l'existence de la variable (créer 'pommes' ?)
+    Put_line("On affecte le nombre 5 à un entier nommé 'pommes' (pommes <- 5)");
+    Put_line("L'affectation doit être réussie");
+    affectation("pommes <- 5");
     New_Line;
 
     --***** test TantQue *****
