bcaf5ac18b1e58ae0b47f397815cf42510d655b8
Eliott <eliott.duclos@etu.toulouse-inp.fr>
2022-01-06 08:50:26 +0100

    1

--- Code/2.InterversExec/interversexec.adb
+++ Code/2.InterversExec/interversexec.adb
@@ -3,14 +3,14 @@ package body interversexec is
 
    procedure intermediaire2execution(code_intermediaire : in File_Type) is
    begin
-        declarationVariables(code_intermediaire);
+        blocDeclaration(code_intermediaire);
         blocPrincipal(code_intermediaire);
    end intermediaire2execution;
 
    procedure blocDeclaration(code_intermediaire : in File_Type) is
 
-     tab_cell_entier : Tableau_Cellule;
-     tab_entier : T_Variables := ("entier",tab_cell_entier);
+     --tab_cell_entier : Tableau_Cellule;
+     --tab_entier : T_Variables := ("entier",tab_cell_entier);
 
    begin
 
@@ -23,7 +23,9 @@ package body interversexec is
         null;
     end blocPrincipal;
 
-    procedure affectation(ligne: in String) is
+    procedure affectation(ligne : in String) is
+
+      a : String := ligne;
 
     begin -- affectation
 
