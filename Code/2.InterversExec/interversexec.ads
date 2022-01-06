60d2d6d2abd8ebfea03669118204549064f0b0d7
camille4426 <camilleperrin26@gmail.com>
2022-01-06 08:53:35 +0100

    c

--- Code/2.InterversExec/interversexec.ads
+++ Code/2.InterversExec/interversexec.ads
@@ -9,6 +9,7 @@ generic
 package interversexec is
 
    --Type index is private;
+   Type String is private;
    Type Tableau_Cellule is private;
    --Type Tab_Entier is private; ##A ENLEVER
    --Type Tab_Booleens is private; ##A ENLEVER
