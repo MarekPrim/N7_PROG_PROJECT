70dd4e6d7e1d1cd27c2a5b21337235fe6cddc921
camille4426 <camilleperrin26@gmail.com>
2022-01-10 11:03:37 +0100

    c

--- Code/2.InterversExec/test_01_affectation.adb
+++ Code/2.InterversExec/test_01_affectation.adb
@@ -14,7 +14,9 @@ procedure test_01_affectation is
     use booleens;
     --Fin packages de généricité (pour tableaux)
 
-    Test : Cellule;
+    test_entiers : T_Variables;
+    test_bool : T_Variables;
+    pommes : integer;
 
 begin
 
@@ -23,25 +25,52 @@ begin
     New_Line;
 
     -- Ce test ne vérifie pas la création de variables via déclaration, seulement leur affectation.
-    -- On crée donc une variable pour ce test :
-    Test := new Cellule;
-    --Test.all.nom := "pommes";
-    --Test.all.valeur := 0;
-    --
+    -- On crée donc une variable d'entiers pour ce test :
+    
+    test_entiers := new T_Variables;
+    test_entiers.all.tab := new Tableau_Cellule;
+    test_entiers.all.nom := "entier";
+    test_entiers.all.tab := ("pommes" => 0);
 
     -- Affection d'un entier à un entier : REUSSITE ATTENDUE    
     Put_line("On affecte le nombre 5 à un entier nommé 'pommes' (pommes <- 5).");
     Put_line("L'affectation doit être réussie :");
     Put("Avant affectation, pommes vaut : ");
-    --Put (pommes, 0);
+    pommes := test_entiers.all.tab("pommes"); --récupération de la valeur actuelle de "pommes"
+    Put (pommes, 0);
     New_Line;
-    --affectation("1 pommes <- 5");
+    interversexec.affectation("1 pommes <- 5");
     Put("Après affectation, pommes vaut : ");
-    --Put (pommes, 0);
+    Put (pommes, 0);
     New_Line;
     New_Line;
 
     -- Affectation d'un string à un entier : ERREUR ATTENDUE
+    Put_line("On affecte le string 'cinq' à un entier nommé 'pommes' (pommes <- 5).");
+    Put_line("L'affectation doit être un échec : la nouvelle valeur n'est pas un entier :");
+    Put("Avant affectation, pommes vaut : ");
+    Put (pommes, 0);
+    New_Line;
+    interversexec.affectation("3 pommes <- cinq");
+    Put("Après affectation, pommes vaut : ");
+    Put (pommes, 0);
+    New_Line;
+    New_Line;
+
+    -- Affection de true à un booléen : REUSSITE ATTENDUE    
+    Put_line("On affecte 'true' à un booleen nommé 'pommes' (pommes <- 5).");
+    Put_line("L'affectation doit être réussie :");
+    Put("Avant affectation, pommes vaut : ");
+    pommes := test_entiers.all.tab("pommes"); --récupération de la valeur actuelle de "pommes"
+    Put (pommes, 0);
+    New_Line;
+    interversexec.affectation("1 pommes <- 5");
+    Put("Après affectation, pommes vaut : ");
+    Put (pommes, 0);
+    New_Line;
+    New_Line;
+
+    -- Affectation d'un entier à un booléen : ERREUR ATTENDUE
     Put_line("On affecte le string 'cinq' à un entier nommé 'pommes' (pommes <- 5).");
     Put_line("L'affectation doit être un échec : la nouvelle valeur n'est pas un entier :");
     Put("Avant affectation, pommes vaut : ");
