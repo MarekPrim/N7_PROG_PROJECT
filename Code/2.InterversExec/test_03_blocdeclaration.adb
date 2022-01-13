70e883d96108e9c3ba238729c8867545eaf1408d
Eliott <eliott.duclos@etu.toulouse-inp.fr>
2022-01-13 09:31:29 +0100

    1

--- Code/2.InterversExec/test_03_blocdeclaration.adb
+++ Code/2.InterversExec/test_03_blocdeclaration.adb
@@ -5,13 +5,6 @@ with interversexec ;
 with genericite ;
 
 procedure test_03_blocdeclaration is
-    -- Packages de généricité (pour tableaux)
-    package entiers is new interversexec(Element=> Integer,image => Integer'Image);
-    use entiers;
-
-    package booleens is new interversexec(Element=> Boolean,image => Boolean'Image);
-    use booleens;
-    --Fin packages de généricité (pour tableaux)
 
     fichier_test : String := "test_files/testE";
 
